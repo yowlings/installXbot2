5dfd409ea4b8ed307aec41181ce10b3d2dbedb63
yowlings@gmail.com <yowlings@gmail.com>
2018-01-02 15:20:26 +0800

    up

--- installXbot2_x64.sh
+++ installXbot2_x64.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #二、安装基础工具软件
 sudo apt-get update
-sudo apt-get install firefox -y
 sudo apt-get install git -y
 
 #四、对机器人端口进行映射
@@ -37,17 +36,13 @@ source ~/.bashrc
 # Install rosinstall
 # 七、安装xbot ROS驱动包依赖的软件
 sudo apt-get install python-rosinstall -y
-sudo apt-get install ros-kinetic-angles -y
-sudo apt-get install ros-kinetic-ecl -y
-sudo apt-get install ros-kinetic-tf -y
 sudo apt-get install ros-kinetic-yocs-cmd-vel-mux -y
-sudo apt-get install ros-kinetic-xacros -y
-sudo apt-get install ros-kinetic-robot-state-publisher -y
-sudo apt-get install ros-kinetic-diagnostic-updater -y
-sudo apt-get install ros-kinect-controller-mannager -y
-sudo apt-get install ros-kinect-gazebo-ros -y
-sudo apt-get install ros-kinect-rqt-plot -y
-sudo apt-get install ros-kinect-rviz -y
+sudo apt-get install ros-kinetic-controller-mannager
+sudo apt-get install ros-kinetic-move-base
+sudo apt-get install ros-kinetic-move-base-msgs
+sudo apt-get install ros-kinetic-hector-slam
+sudo apt-get install ros-kinetic-gmapping
+sudo apt-get install ros-kinetic-robot-upstart
 
 ./setupCatkinWorkspace.sh
 
@@ -56,8 +51,14 @@ sudo apt-get install ros-kinect-rviz -y
 cd 
 git clone https://github.com/DroidAITech/xbot2 ~/catkin_ws/src/xbot2/
 git clone https://github.com/DroidAITech/xbot2_description ~/catkin_ws/src/xbot2_description/
+git clone https://github.com/robopeak/rplidar_ros.git ~/catkin_ws/src/rplidar_ros/
+source ~/.bashrc
 cd ~/catkin_ws
 catkin_make
+rosrun robot_upstart install xbot_bringup/launch/xbot_websocket.launch
+sudo systemctl daemon-reload
+sudo systemctl start xbot
+
 
 
 

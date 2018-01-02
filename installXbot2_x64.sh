ec7197b5c648c5d989c0abcd4143a69d50b70a20
yowlings@gmail.com <yowlings@gmail.com>
2018-01-02 18:47:09 +0800

    streatvideo

--- installXbot2_x64.sh
+++ installXbot2_x64.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#二、安装基础工具软件
+echo "开始安装基础工具软件......"
 sudo apt-get update
 sudo apt-get install git -y
 sudo apt-get install ssh -y
-#四、对机器人端口进行映射
+echo "开始对机器人端口进行映射......"
 sudo cp 58-xbot.rules /etc/udev/rules.d/
 sudo service udev restart
-
+echo "加入ROS安装库......"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 # Installation
 sudo apt-get update
 sudo apt-get install ros-kinetic-ros-desktop-full -y
-# Add Individual Packages here
+# Add Individual Packagejis here
 # You can install a specific ROS package (replace underscores with dashes of the package name):
 # sudo apt-get install ros-kinetic-PACKAGE
 # e.g.
@@ -37,13 +37,13 @@ source ~/.bashrc
 # 七、安装xbot ROS驱动包依赖的软件
 sudo apt-get install python-rosinstall -y
 sudo apt-get install ros-kinetic-yocs-cmd-vel-mux -y
-sudo apt-get install ros-kinetic-controller-mannager
-sudo apt-get install ros-kinetic-move-base
-sudo apt-get install ros-kinetic-move-base-msgs
-sudo apt-get install ros-kinetic-hector-slam
-sudo apt-get install ros-kinetic-gmapping
-sduo apt-get install ros-kinetic-dwa-local-planner
-sudo apt-get install ros-kinetic-robot-upstart
+sudo apt-get install ros-kinetic-controller-mannager -y
+sudo apt-get install ros-kinetic-move-base -y
+sudo apt-get install ros-kinetic-move-base-msgs -y
+sudo apt-get install ros-kinetic-hector-slam -y
+sudo apt-get install ros-kinetic-gmapping -y
+sduo apt-get install ros-kinetic-dwa-local-planner -y
+sudo apt-get install ros-kinetic-robot-upstart -y
 
 ./setupCatkinWorkspace.sh
 
@@ -53,7 +53,7 @@ cd
 git clone https://github.com/DroidAITech/xbot2 ~/catkin_ws/src/xbot2/
 git clone https://github.com/DroidAITech/xbot2_description ~/catkin_ws/src/xbot2_description/
 git clone https://github.com/robopeak/rplidar_ros.git ~/catkin_ws/src/rplidar_ros/
-sudo cp ~/catkin_ws/src/rplidar_ros/scripts/rplidar.rules /etc/udev/rules/57-rplidar.rules
+sudo cp ~/catkin_ws/src/rplidar_ros/scripts/rplidar.rules /etc/udev/rules.d/57-rplidar.rules
 sudo service udev restart
 source ~/.bashrc
 cd ~/catkin_ws

520884c11d1d66302b25f2d91e4ab6ad889d26c5
yowlings@gmail.com <yowlings@gmail.com>
2018-04-19 16:40:46 +0800

    add xbot-u.sh

---
+++ xbot-u.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/.bashrc
+roslaunch xbot_bringup xbot_websocket.launch & ffmpeg -f v4l2  -i /dev/video2 -r 25 -b:v 400k -vcodec h264 -preset superfast -tune zerolatency -an  -f flv rtmp://localhost/rgb

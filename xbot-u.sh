dfce2dc4aae3e8df269e656cc3e49451538c5543
yowlings@gmail.com <yowlings@gmail.com>
2018-04-23 08:44:32 +0800

    stable1.0

--- xbot-u.sh
+++ xbot-u.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ~/.bashrc
-roslaunch xbot_bringup xbot_websocket.launch & ffmpeg -f v4l2  -i /dev/video2 -r 25 -b:v 400k -vcodec h264 -preset superfast -tune zerolatency -an  -f flv rtmp://localhost/rgb
+roslaunch xbot_bringup xbot_rplidar_websocket.launch & ffmpeg -f v4l2  -i /dev/video2 -r 25 -b:v 400k -vcodec h264 -preset superfast -tune zerolatency -an  -f flv rtmp://localhost/rgb
